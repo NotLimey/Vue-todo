46170f760c43b12369cb7161cbfe34d90ad5422c
Martin Myhre <70258183+NotLimey@users.noreply.github.com>
2022-04-25 09:12:40 +0200

    Vue

---
+++ src/App.vue
@@ -0,0 +1,66 @@
+<script>
+import { TrashIcon, PlusSmIcon } from '@heroicons/vue/solid'
+
+export default {
+  name: 'App',
+  components: { 
+    PlusSmIcon,
+    TrashIcon },
+  data() {
+    return {
+      todos: [],
+      todo: "",
+    }
+  },
+  methods: {
+    removeTodo(index) {
+      this.todos.splice(index, 1)
+    },
+    addTodo() {
+      this.todos.push(this.todo);
+      this.todo = "";
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="flex flex-col items-center m-10">
+    <h1 class="text-3xl">Todos</h1>
+    <div class="flex gap-2 my-4">
+      <div>
+      <label htmlFor="text" class="sr-only">
+        Todo
+      </label>
+      <input
+        type="text"
+        name="text"
+        id="text"
+        class="shadow-sm focus:ring-green-500 focus:border-green-500 block w-full sm:text-sm border-stone-700 rounded-md bg-stone-800"
+        placeholder="Todo.."
+        :value="todo"
+        @input="event => todo = event.target.value"
+      />
+    </div>
+    <div class="flex items-center">
+      <button
+        type="button"
+        class="inline-flex items-center p-1 border border-transparent rounded-full shadow-sm text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500"
+        @click="addTodo()"
+      >
+        <PlusSmIcon class="h-5 w-5" aria-hidden="true" />
+      </button>
+    </div>
+    </div>
+    <div class="max-w-sm w-full mx-5 mt-4">
+      <div v-for="(todo, index) in todos" :key="todo" class="w-full bg-red-500 rounded-sm py-2 px-4 mb-4 flex justify-between">
+        <h2>{{todo}}</h2>
+        <TrashIcon class="h-6 w-6 cursor-pointer" @click="removeTodo(index)" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+@import "./assets/base.css";
+</style>
